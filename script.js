84c2d60e2bcd17a24537b06b27a05198f8a205ea
Samuele <samuele.gugliotta23@gmail.com>
2022-09-22 12:00:48 +0200

    Creazione Cronometro

--- script.js
+++ script.js
@@ -5,16 +5,32 @@ var count = 0;
 
 currentCount();
 
+function currentCount() {
+    counter.innerText = count;  
+}
+
 decrement.addEventListener('click', ()=> {
     count--;
     currentCount();
 })
 
+
 increment.addEventListener('click', ()=> {
     count++;
-    currentCount();
+    currentCount();  
 })
 
-function currentCount() {
-    counter.innerText = count;
+
+let valueArray = []
+function getValue() {
+    let value = document.getElementById("submit").value
+    valueArray.push(value)
+    counter.innerText = valueArray;
+    document.getElementById("submit").value=""; //Ripulisce la casella dopo aver confermato il valore
+    valueArray.pop(); // Ripulisce l'array, consentendo l'inserimento di un solo valore
 }
+
+
+
+    
+    
